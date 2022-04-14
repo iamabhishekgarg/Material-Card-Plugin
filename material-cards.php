55579ac615e7554f3e3fbfb37001f9b4ed136c94
Abhishek Garg <59972803+iamabhishekgarg@users.noreply.github.com>
2022-04-14 14:17:28 +0530

    Add files via upload

---
+++ material-cards.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:       Material Cards
+ * Plugin URI:        https://iamabhishekgarg.in/
+ * Description:       Beautful Material Cards widget for Elementor.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Abhishek Garg
+ * Author URI:        https://iamabhishekgarg.in/c
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wpac-material-cards
+ */
+if( ! defined ('ABSPATH') ) {
+    exit;
+}
+
+/**
+ * Widgets Loader
+ */
+
+require plugin_dir_path(__FILE__).'widgets-loader.php';
